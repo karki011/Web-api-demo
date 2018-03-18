4a1af05a6ec32850a6c23279ec104ef14e6e4aac
subash karki <karki011@gmail.com>
2018-03-18 15:45:25 -0500

    Init

---
+++ webApi/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace webApi.Models
+{
+    public class Product
+    {     
+        public int Id { get;set;}
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+}
